7819aaf9e90de29d0173875a38b79c158a6d7b64
Ji Li <liji15@mails.tsinghua.edu.cn>
2018-11-23 06:31:16 +0000

    automatically backed up by learn

--- lib/my_all.rb
+++ lib/my_all.rb
@@ -1,6 +1,9 @@
 require 'pry'
 
 def my_all?(collection)
+  if collection.length == 0
+    return false
+  end
   i = 0
   while i < collection.length
     block_return_values << yield(collection[i])
